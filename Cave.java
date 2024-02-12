88c231b715c4546512e4b2060d6921000e6901ee
billybobjoeyeet <1110094@apps.nsd.org>
2024-02-12 13:09:49 -0800

    new file:   Main.java

---
+++ Cave.java
@@ -0,0 +1,14 @@
+// Ishaan and Anders are working on this
+// Period 5 CSA
+//2/12/24
+//Hunt The Wumpus
+public class Cave {
+    //Properties
+
+    //Constructor(s)
+    public Cave(){
+        
+    }
+    //Methods
+
+}
