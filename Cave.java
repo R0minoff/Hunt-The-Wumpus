8c19958500f6c9777dd24ae5e20357e4315ea7ae
Anders0926 <1079942@apps.nsd.org>
2024-02-16 13:32:38 -0800

    new file:   Cave.java 	new file:   Main.java 	new file:   README.md 	new file:   Sound.java 	new file:   Trivia.java 	new file:   gameControl.java

---
+++ Cave.java
@@ -0,0 +1,20 @@
+// Ishaan and Anders are working on this
+// Period 5 CSA
+// 2/12/24
+// Hunt The Wumpus - Cave Class
+
+public class Cave {
+    ///////////////////////
+    // Properties & Fields
+    //////////////////////
+
+    ///////////////////////
+    // Constructor(s)
+    //////////////////////
+    public Cave(){
+        
+    }
+    ///////////////////////
+    // Methods
+    //////////////////////
+}
