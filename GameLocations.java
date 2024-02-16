daa1f22e9439feafac02d34e13e9622ac7b76291
Amrit Gill <amritgill0428@outlook.com>
2024-02-16 13:25:06 -0800

    new file:   GameLocations.java

---
+++ GameLocations.java
@@ -0,0 +1,20 @@
+// Aadhav and Arunim
+// February 16, 2024
+// Period 5
+// Hunt the Wumpus
+
+public class GameLocations {
+    ///////////////////////
+    // Properties & Fields
+    //////////////////////
+
+    ///////////////////////
+    // Constructor(s)
+    //////////////////////
+    public GameLocations(){
+
+    }
+    ///////////////////////
+    // Methods
+    //////////////////////
+}
