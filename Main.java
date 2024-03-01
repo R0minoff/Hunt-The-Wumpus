c6a10d00004a74e9b900bd1acb1902fa600e7380
ArunimG1207 <2012778@apps.nsd.org>
2024-03-01 13:30:14 -0800

    modified:   Main.java

--- Main.java
+++ Main.java
@@ -1,3 +1,8 @@
+// Arunim, Amrit, Aadhav, Ishaan, Eyoeul, Mateen, Kjell, Anders
+// Period 5 CSA
+// 2/12/24
+// Hunt The Wumpus - Main Class
+
 public class Main{
     public static void main(String[] args){
         System.out.println("Hello world");
