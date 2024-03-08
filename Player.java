6a44ce7cff5008d5c3179e2ed6d5dea7678b08ed
Amrit Gill <amritgill0428@outlook.com>
2024-03-08 12:55:06 -0800

    modified:   Player.java

--- Player.java
+++ Player.java
@@ -3,10 +3,13 @@
 // Period 5
 // Hunt the Wumpus
 
+import java.util.Scanner;
+
 public class Player {
     ///////////////////////
     // Properties & Fields
     //////////////////////
+    private String name;
 
     ///////////////////////
     // Constructor(s)
@@ -17,4 +20,14 @@ public class Player {
     ///////////////////////
     // Methods
     //////////////////////
+
+    // This method gets the name of the Player
+    public String getName(){
+        Scanner s = new Scanner(System.in);
+        System.out.println("What is your name?");
+        name = s.nextLine();
+        System.out.println("Hello " + name + "! Welcome to Hunt the Wumpus!");
+
+        return name;
+    }
 }
