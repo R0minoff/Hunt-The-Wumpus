617bb9380fab9dfcc35b664579e47b9e6b5e3994
Anders0926 <1079942@apps.nsd.org>
2024-03-14 12:22:30 -0700

    modified:   HuntTheWumpus/Cave/Cave.java 	modified:   HuntTheWumpus/Cave/CaveTest.java 	new file:   HuntTheWumpus/Cave/Maps.csv

--- HuntTheWumpus/Cave/Cave.java
+++ HuntTheWumpus/Cave/Cave.java
@@ -13,18 +13,61 @@ import HuntTheWumpus.Trivia.*;
 import HuntTheWumpus.UI.*;
 import HuntTheWumpus.Wumpus.*;
 
-public class Cave {
+import java.util.Scanner;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+
+public class Cave  {
     ///////////////////////
     // Properties & Fields
     //////////////////////
-
+    File file;
+    Scanner reader;
+    String line;
     ///////////////////////
     // Constructor(s)
     //////////////////////
-    public Cave(){
-        
+    public Cave() throws FileNotFoundException {
+        this.file = new File("Maps.csv");
+        this.reader = new Scanner(this.file);
     }
     ///////////////////////
     // Methods
     //////////////////////
+   public String[] ReadCaveFile() throws FileNotFoundException {
+    //this one will process information from the cave file
+    //so far it should be a CSV which has information of the map
+
+    //skips the header
+    reader.nextLine();
+    //reads the first line in the csv
+    String line = reader.nextLine();
+    //when we had the map back we will put it at the very end, meaning that every time it will go through a new map
+    String[] info = line.split(","); 
+
+    //returns the info found by the first line with actual information
+    return info;
+
+   }
+   public void WriteCaveFile() throws FileNotFoundException {
+    //this one will write the played map back into the file (at the end)
+    try{
+      FileWriter FW = new FileWriter(file, true);
+      FW.write(ConvertInfo(ReadCaveFile()));
+      FW.close();
+    } catch (Exception e) {
+
+      System.out.println("i suck " + e);
+
+    }
+   }
+   public String ConvertInfo(String[] info){
+    //this converts the information from the string[] into string
+    String str = "";
+    for(int i = 0; i < info.length; i++){
+        str += info[i];
+    }
+    return str;
+   }
 }
