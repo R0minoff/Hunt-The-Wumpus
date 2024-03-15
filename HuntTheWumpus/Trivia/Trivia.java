efac765212f4832a5dcf0a8c0fc443259ccc53e4
Kjell224 <1108721@apps.nsd.org>
2024-03-14 21:19:14 -0700

    modified:   HuntTheWumpus/Trivia/Questions.csv 	modified:   HuntTheWumpus/Trivia/Trivia.java

--- HuntTheWumpus/Trivia/Trivia.java
+++ HuntTheWumpus/Trivia/Trivia.java
@@ -12,26 +12,55 @@ import HuntTheWumpus.Sound.*;
 import HuntTheWumpus.Trivia.*;
 import HuntTheWumpus.UI.*;
 import HuntTheWumpus.Wumpus.*;
+import java.util.Scanner;
+import java.io.FileNotFoundException;
+import java.io.File;
+import java.io.IOException;
 
 public class Trivia{
     ///////////////////////
     // Properties & Fields
     //////////////////////
+    private File file;
+    private int difficulty;
+    private String question;
+    private String answer;
 
     ///////////////////////
     // Constructor(s)
     //////////////////////
+
     public Trivia(){
-        
+        this.file = new File("Questions.csv");
+        getQuestion();
     }
+
     ///////////////////////
     // Methods
     //////////////////////
 
     public String getQuestion(){
-        String question = "";
-        return question;
+        String myQuestion = "";
+        try{
+            Scanner s = new Scanner(this.file);
+            String line = "";
+            while(s.hasNextLine()){
+                s.next();
+                line = s.nextLine();
+                String[] info = line.split(",");
+                this.question = info[0];
+                this.difficulty = Integer.parseInt(info[1]);
+                this.answer = info[2];
+              System.out.println(Arrays.toString(info));
+            }
+        } catch(FileNotFoundException e){
+            System.out.println("File not Found");
+        }
+        return myQuestion;
     }
+
+
+
 
     public boolean askQuestion(){
         boolean correct = false;
