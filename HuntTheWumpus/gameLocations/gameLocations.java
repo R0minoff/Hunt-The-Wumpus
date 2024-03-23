1a6c927b59c252aad1703774f17a65db87d9184b
ArtunimG1207 <2012778@apps.nsd.org>
2024-03-23 12:04:41 -0700

    modified:   HuntTheWumpus/gameLocations/gameLocations.java

--- HuntTheWumpus/gameLocations/gameLocations.java
+++ HuntTheWumpus/gameLocations/gameLocations.java
@@ -3,7 +3,37 @@
 // Period 5
 // Hunt the Wumpus - Game Locations Class
 
-/* */
+/*
+ *  ABOUT THE OBJECT  *
+ *  The gameLocations object tracks the locations of all objects in the current game. The tasks it performs are as follows:
+ *      - Store and interact with the cave used for this game
+ *      - Keep track of where the hazards are
+ *      - Keep track of where the wumpus is
+ *      - Keep track of where the player is
+ *      - Control arrow shooting
+ *      - Give any necessary warnings
+ *      - Obtain hints to help the player
+ *  MORE INFO  *
+ *   Hazards (Wumpus is immune to all hazards):
+ *      - Bottomless Pits
+ *          - Two rooms have bottomless pits in them
+ *          - You can save yourself by answering 2/3 trivia questions correctly
+ *          - If you escape the pit, you will spawn back where you started the game
+ *      - Super Bats
+ *          - Two rooms have super bats in them
+ *          - The bats will take you to some room at random
+ *          - They fly away into another random room in the cave after taking you
+ *      - No room has more than one hazard
+ *      - The wumpus is not considered as a hazard
+ *   Warnings:
+ *      - You will be given a warning whenever a hazard or wumpus is in an adjacent room
+ *          - Wumpus: I smell a Wumpus!
+ *          - Bat: Bats Nearby.
+ *          - Pit: I feel a draft.
+ *   Arrows: 
+ *      - Player starts game with 3 arrows
+ *      - Shoot arrows into any adjacent room connected by a tunnel
+ */
 
 package HuntTheWumpus.gameLocations;
 import HuntTheWumpus.Cave.*;
