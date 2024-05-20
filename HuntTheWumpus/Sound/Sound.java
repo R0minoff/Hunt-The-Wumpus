0143fc32c1924bd3377803caca2679cb984dbf7d
Amrit Gill <amritgill0428@outlook.com>
2024-05-20 13:23:01 -0700

    modified:   HuntTheWumpus/Sound/Sound.java

--- HuntTheWumpus/Sound/Sound.java
+++ HuntTheWumpus/Sound/Sound.java
@@ -27,15 +27,27 @@ public class Sound {
     // Constructor(s)
     //////////////////////
     public Sound(){
-        WrongSound();
+        
     }
     ///////////////////////
     // Methods
     //////////////////////
 
+    public void BackgroundSound(){
+        File background = new File("HuntTheWumpus/Sound/mixkit-arcade-retro-background-219.wav");
+
+        try{
+            Clip clip = AudioSystem.getClip();
+            clip.open(AudioSystem.getAudioInputStream(background));
+            clip.start();
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
     // This method is the sound for when the player dies
     public void DeathSound(){
-        File death = new File("");
+        File death = new File("HuntTheWumpus/Sound/NSKF2KH-chime-lose.mp3");
     
 
         try{
@@ -49,7 +61,7 @@ public class Sound {
 
     // This method is the sound for when the player beats the Wumpus
     public void WinSound(){
-        File win = new File("");
+        File win = new File("HuntTheWumpus/Sound/mixkit-video-game-win-2016.wav");
     
 
         try{
@@ -63,7 +75,7 @@ public class Sound {
 
     // This method is the sound for when the player gets a trvia question wrong.
     public void WrongSound(){
-        File wrong = new File("HuntTheWumpus/Sound/mixkit-arcade-retro-background-219.wav");
+        File wrong = new File("HuntTheWumpus/Sound/BQY5HXR-wrong.mp3");
 
         try{
             Clip clip = AudioSystem.getClip();
@@ -76,7 +88,7 @@ public class Sound {
 
     // This method is the sound for when the player gets a trivia question right.
     public void CorrectSound(){
-        File correct = new File("");
+        File correct = new File("HuntTheWumpus/Sound/mixkit-correct-answer-reward-952.wav");
 
         try{
             Clip clip = AudioSystem.getClip();
