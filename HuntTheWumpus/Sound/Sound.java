c52f475029e345424882a8ea6f807f7b071080d0
Amrit Gill <amritgill0428@outlook.com>
2024-05-21 12:37:40 -0700

    modified:   HuntTheWumpus/Sound/Sound.java

--- HuntTheWumpus/Sound/Sound.java
+++ HuntTheWumpus/Sound/Sound.java
@@ -27,7 +27,7 @@ public class Sound {
     // Constructor(s)
     //////////////////////
     public Sound(){
-        
+        BackgroundSound();
     }
     ///////////////////////
     // Methods
