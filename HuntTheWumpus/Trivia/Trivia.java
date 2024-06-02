7d81dd9bc5f74a0912a6c78e5c1911554085f953
Kjell224 <1108721@apps.nsd.org>
2024-06-02 12:31:00 -0700

    modified:   HuntTheWumpus/Trivia/Trivia.java

--- HuntTheWumpus/Trivia/Trivia.java
+++ HuntTheWumpus/Trivia/Trivia.java
@@ -8,7 +8,6 @@ import java.util.Scanner;
 import java.io.FileNotFoundException;
 import java.io.File;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Collections;
 import java.io.FileWriter;
 
